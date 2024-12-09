d37a6e1c5b8bdfdbb3e1da514b7b5cc00d4afb9b
BlackKnightZERO <ayon.arif.10@gmail.com>
2024-12-09 15:54:20 +0600

    next-form-added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
+import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams} : {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query
   return (
     <>
-        <h1 className="text-2xl">HOME</h1>
+        <section className="pink_container">
+          <h1 className="heading">Pitch your startup
+            <br />
+            Connect with Enterpreneurs
+          </h1>
+          <p className="sub-heading !max-w-3xl">
+            Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Completitions
+          </p>
+          <SearchForm query={query} />
+        </section>
     </>
   );
 }
