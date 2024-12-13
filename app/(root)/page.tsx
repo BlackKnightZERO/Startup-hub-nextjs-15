93c1eac38269d089a020db0b1402102990e6dab4
BlackKnightZERO <ayon.arif.10@gmail.com>
2024-12-13 16:30:26 +0600

    sanity-setup-fetch-data-completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 
 export default async function Home({searchParams} : {
@@ -7,28 +9,7 @@ export default async function Home({searchParams} : {
 }) {
   const query = (await searchParams).query
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name:'Arif' },
-      _id: 1,
-      description: "Description",
-      image: "https://plus.unsplash.com/premium_photo-1732730224306-3b469ea9e640?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Robots",
-      title: "We Robots"
-    },
-    {
-      _createdAt: new Date(),
-      views: 45,
-      author: { _id: 2, name:'Faysal' },
-      _id: 2,
-      description: "Description",
-      image: "https://images.unsplash.com/photo-1731582338361-f5d4101e3a25?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      category: "Laptops",
-      title: "We Laptops"
-    },
-  ]
+  const posts = await client.fetch(STARTUPS_QUERY)
 
   return (
     <>
